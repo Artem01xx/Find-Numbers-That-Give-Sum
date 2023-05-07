126974c4a6a3744ae889f070c24e903f212e2dfe
Artem01xx <131177303+Artem01xx@users.noreply.github.com>
2023-05-07 14:02:13 +0500

    Create Programming.cpp

---
+++ Programming.cpp
@@ -0,0 +1,31 @@
+﻿// Programming.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+int main()
+{
+	const int size = 5;
+	int arr[] = { 2,4,6,5,9 };
+	int sum = 6;
+
+	for (int i = 0;i < size; i++)
+	{
+		for (int j = i + 1; j<size ; j++)
+		{
+			if (arr[i]+arr[j] == sum)
+			{
+				cout << arr[i] << " " << arr[j] << endl;
+			}
+			
+		}
+	}
+
+	return 0;
+}
